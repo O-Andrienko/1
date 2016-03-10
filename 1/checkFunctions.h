cdbad872eef20e24a501ed9db123d9e0656ae385
O-Andrienko <skorp-olya@ukr.net>
2016-03-10 21:50:19 +0200

    Added checking parameters on region of permissible values

---
+++ 1/checkFunctions.h
@@ -0,0 +1,1 @@
+bool checkparams(float a, float b, float c);
--- 1/main.cpp
+++ 1/main.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "InputFunctions.h"
+#include "checkFunctions.h"
 
 void main()
 {
+	
 	float a, b, c;
 	inputParams(&a, &b, &c);
+	while (checkparams(a, b, c) != true) inputParams(&a, &b, &c);
+
 	getchar();
 }
 
